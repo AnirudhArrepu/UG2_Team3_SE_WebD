4358d8a157936a018900b5ab8475d1cc41060700
AnirudhArrepu <anirudharrepu@gmail.com>
2024-11-17 22:25:11 +0530

    reset password deploy

--- WebD/server/routes/user.js
+++ WebD/server/routes/user.js
@@ -83,7 +83,7 @@ router.post("/forgot-password", async (req, res) => {
       from: "your_email@gmail.com",
       to: email,
       subject: "Reset Password",
-      text: `http://localhost:5173/resetPassword/${token}`,
+      text: `https://ug2-team3-se-webd-1.onrender.com/resetPassword/${token}`,
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
